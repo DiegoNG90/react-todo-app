96557d5cf5543c7b58e194137840ffd77abd5b41
Diego <diego.nicolas.gandara@gmail.com>
2021-03-05 20:10:32 -0300

    Se importa styles.css y Form. Se pasa al return el componente importado Form

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import './styles.css';
+import Form from './Form';
 
 const App = () => {
-    return <div>App</div>
+    return (
+    <div>
+        <h1> To-do App </h1> 
+        <Form />
+    </div>
+    )
 }
 
 export default App;
