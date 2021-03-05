807dfc948ee075bb7753cc37a2521fb56d85bfdd
Diego <diego.nicolas.gandara@gmail.com>
2021-03-05 20:49:48 -0300

    Se estructura un poquito el return, pero de forma provisoria, para chequear por patanlla como va quedando

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './styles.css';
 import Form from './Form';
 
+import TodoList from './TodoList'
+
 const App = () => {
     return (
-    <div>
+    <div className="container">
         <h1> To-do App </h1> 
         <Form />
+        <TodoList />       
     </div>
     )
 }
