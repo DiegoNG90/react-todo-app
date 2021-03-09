e2fb758d6f025fd220c2591ab8903ccd4fb7b216
Diego <diego.nicolas.gandara@gmail.com>
2021-03-09 17:22:19 -0300

    Primero, creamos un evento onClick dentro del boton .delete-btn que está asociada a la funcion deleteHandler. Ahora, vamos al cuerpo de la funcion y la definimos.La logica es: tenemos que setear el state de los TodoItems. Entonces llamamos a setTodoItems y pasamos una condicion logica a su interior: recorriendo la lista de todoItems -aquellos que ya estan guardados en el state- preguntamos si el id del recorrido NO coincide con el id del recorrido. Si es falso, no pasa nada. Pero sino, lo remueve del state, es decir, de todoItems

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
-const TodoItem = ({task}) =>{
-    // console.log(todoItems);
+const TodoItem = ({task,item,todoItems, setTodoItems}) =>{
+    //Delete TodoItem
+    const deleteHandler = () => {
+        setTodoItems(todoItems.filter((element) => element.id !== item.id));
+        // console.log(item);
+    }
     return (
         <div className="todo-item d-flex justify-content-between">
             <p>{task}</p>
             <div className="todo-item-btn d-flex justify-content-end align-items-start">
                 <button className="complete-btn"><i className="fas fa-check"></i></button>
-                <button className="delete-btn"><i className="fas fa-trash"> </i></button>
+                <button 
+                    className="delete-btn"
+                    onClick={deleteHandler}
+                >
+                        <i className="fas fa-trash"> </i></button>
             </div>
         </div>
     )
