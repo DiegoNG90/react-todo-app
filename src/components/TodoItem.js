4bf8215ecd67445e6bcd2ae47f5e082c2a874a77
Diego <diego.nicolas.gandara@gmail.com>
2021-03-09 18:43:49 -0300

    Se recorre nuevamente el array de todoItems y se comprueba si el elemento actual que está recorriendo corresponde con el item seleccionado. Si es así, la funcion nos va a devolver el elemento mismo, pero cambiando una de sus keys: completed. En caso que no haya correspondencia, que nos devuelva el mismo elemento. Ahora pasamos la funcion -completedHandler- al evento onClick del .complete-btn. Por ultimo, para aplicar el estilo, proponogo un operador ternario en la etiqueta <p> qie chequee el valor booleano de item.completed. Si es true, le aplica los estilos -creado en el commit anterior-. Sino, no aplica ningun estilo

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -6,11 +6,27 @@ const TodoItem = ({task,item,todoItems, setTodoItems}) =>{
         setTodoItems(todoItems.filter((element) => element.id !== item.id));
         // console.log(item);
     }
+    const completedHandler = () => {
+        setTodoItems(todoItems.map((element) => {
+            if(element.id === item.id){
+                return {
+                    ...element,completed: !item.completed
+                }
+            }
+            return element;
+        }))
+    }
     return (
         <div className="todo-item d-flex justify-content-between">
-            <p>{task}</p>
+            <p className={`${item.completed ? "completed": ""}`} >{task}</p>
             <div className="todo-item-btn d-flex justify-content-end align-items-start">
-                <button className="complete-btn"><i className="fas fa-check"></i></button>
+                <button 
+                    className="complete-btn"
+                    onClick={completedHandler}
+                >
+                       <i className="fas fa-check"></i>
+                
+                </button>
                 <button 
                     className="delete-btn"
                     onClick={deleteHandler}
